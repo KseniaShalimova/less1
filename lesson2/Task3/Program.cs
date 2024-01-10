12c5fe4a139b4a3340e45980763abc2b0df6ba5b
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-10 20:21:22 +0500

    add lesson2:Task3

---
+++ lesson2/Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] arr = { 5, 3, 22, 44, 35, 49, 66, 68, 1, 57 };
+int i = 0;
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
