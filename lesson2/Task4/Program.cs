0c7811ed6cdb2403f6b24dca54bb7b4734d72b59
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-10 20:30:58 +0500

    add lesson2:Task4

---
+++ lesson2/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 5;
+int[] arr = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = arr[0];
+while (i < n)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
