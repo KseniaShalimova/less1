af8e976e7fe9cb7004e32332573fcce85fe24cbb
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-10 20:06:51 +0500

    add lesson2:Task2

---
+++ lesson2/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int i = 0;
+int[] arr = new int[n];
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
