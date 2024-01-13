fd0a56d926b4bc189e38bf3393c6c64bcfb0f42f
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-14 01:18:20 +0500

    add seminar3:Task1

---
+++ seminar3/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[] {14,5,34,76,43};
+int target = 14;
+bool flag = false;
+foreach (int e in arr)
+{
+    if (e==target)
+    {
+        Console.WriteLine("Yes");
+        flag = true;
+    }
+}
+
+if (flag == false)
+{
+    Console.WriteLine("No");
+}
