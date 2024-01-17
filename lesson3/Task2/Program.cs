0a53579ca0018f6636493dca4619333552ffe10b
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-17 19:57:34 +0500

    add leson3:Task2

---
+++ lesson3/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintSquares(int Limit)
+{
+    int i = 1;
+    while (i <= Limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+
+}
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
