cca1e5d2797963a6cf71a055cea48a1198ddb14b
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-21 02:20:04 +0500

    add lesson3:Task3

---
+++ lesson4/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLettersFromString (string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string res = GetLettersFromString(str);
+Console.WriteLine(res);
