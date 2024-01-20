6c25c3e8555a783760728e3b307cc69de9e71387
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-21 01:37:57 +0500

    add lesson4:Task1

---
+++ lesson4/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] CreateMatrix(int rowCounts, int columCounts)
+{
+    int[,] matrix = new int[rowCounts, columCounts];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void showMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] matrix = CreateMatrix(4, 5);
+showMatrix(matrix);
