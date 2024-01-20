35564fa6eb33bb4b5a3ad0d87f825bc6f8407ef5
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-21 02:03:40 +0500

    add lesson4:Task2

---
+++ lesson4/Task2/Program.cs
@@ -0,0 +1,61 @@
+﻿int[,] matrix = CreateMatrix(3,4);
+showMatrix(matrix);
+foreach (int e in matrix)
+{
+    if (IsInteresting(e)==true)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+
+
+int[,] CreateMatrix(int rowCounts, int columCounts)
+{
+    int[,] matrix = new int[rowCounts, columCounts];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 1000);
+        }
+    }
+    return matrix;
+}
+
+int GetSumOfDigits(int value)
+{
+    int sum = 0;
+    while (value>0)
+    {
+        sum = sum + value%10;
+        value = value/10;
+    }
+    return sum;
+}
+
+
+
+bool IsInteresting(int value)
+{
+    int sumOfDigits = GetSumOfDigits(value);
+    if (sumOfDigits%2==0)
+    {
+        return true;
+    }
+    return false;
+}
+
+
+void showMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
