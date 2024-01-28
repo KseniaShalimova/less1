cf5f20a0f40c1dbd4ee4464cfe7398a50a0a71cf
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-28 23:34:05 +0500

    add seminar6:Task2

---
+++ seminar6/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿string str = "Hello";
+char[] chars = new char[str.Length];
+for (int i=0; i<str.Length; i++)
+{
+    chars[i]=str[i];
+}
+
+foreach (char e in chars)
+{
+    Console.WriteLine(e);
+}
